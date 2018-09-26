8935db191f0ac7683865a77abdad3cd6201eab7f
Faried-4A6A <514152@edu.rocmn.nl>
2018-09-26 09:57:47 +0200

    edit header

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
+ require_once "view/header.php";
 
-define("APP_DIR", "C:/xampp/htdocs/prog_oefen/");
+define("APP_DIR", "C:/xampp/htdocs/GitHub/gameplay-party/");
 
 require 'model/Router.php';
 
   $Router = new Router();
-
-  
