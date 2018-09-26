a43dbb3a60a5029c7c3e442b9b5706eb2ffbdc75
Faried-4A6A <514152@edu.rocmn.nl>
2018-09-26 12:50:31 +0200

    edit over ons

--- view/over_ons.php
+++ view/over_ons.php
@@ -1,6 +1,9 @@
-<?php require_once "view/header.php"; ?>
-<div class="container">
-    <div class="">
-        <img src="https://via.placeholder.com/350x150" alt="" />
+<?php require_once "header.php"; ?>
+<div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6"><img src="https://via.placeholder.com/860x420" alt="" /></div>
+      <div class="col-sm-6">
+          <h1><b>Over ons</b></h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
 </div>
