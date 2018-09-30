bc40328a3497c6624712f706eedcbf7e84c127a8
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-09-30 20:01:03 +0200

    added controllers and fixed them

---
+++ controller/overonsController.php
@@ -0,0 +1,19 @@
+<?php
+
+class overonsController {
+
+
+    public function __construct(){
+    }
+    public function __destruct() {}
+
+
+    public function overons() {
+        require "view/over_ons.php";
+    }
+
+
+
+}
+
+?>
