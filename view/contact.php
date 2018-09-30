22c3e424f7ba2bfed2a7844101020040904fe7df
Faried-4A6A <514152@edu.rocmn.nl>
2018-09-30 16:04:46 +0200

    contact page design af

--- view/contact.php
+++ view/contact.php
@@ -2,13 +2,17 @@
 
 <div class="container">
     <form>
-      <div class="form-row">
-        <div class="col">
-          <input type="text" class="form-control" placeholder="First name">
-        </div>
-        <div class="col">
-          <input type="text" class="form-control" placeholder="Last name">
-        </div>
+      <div class="form-row justify-content-center">
+          <label><h1>Contact</h1></label>
+      </div>
+      <hr>
+      <div class="form-row justify-content-center">
+          <label>Naam</label>
+          <input type="text" class="form-control">
+          <label>Email</label>
+          <input type="text" class="form-control">
+          <label>Bericht</label>
+          <textarea type="text" class="form-control"></textarea>
       </div>
     </form>
 </div>
