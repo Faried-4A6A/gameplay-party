27b408dc4c941bfb8ad311d90bbcd1e62d38f296
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-10-01 13:52:30 +0200

    login

---
+++ view/login.php
@@ -0,0 +1,18 @@
+<?php require_once 'partials/header.php'; ?>
+
+<div class="container">
+    <form>
+      <div class="form-row mt-5 justify-content-center">
+          <label><h1>login</h1></label>
+      </div>
+      <hr>
+      <div class="form-row justify-content-center">
+          <label>Naam</label>
+          <input type="text" class="form-control">
+          <label>wachtwoord</label>
+          <input type="text" class="form-control">          
+      </div>
+    </form>
+</div>
+
+<?php require_once 'partials/footer.php'; ?>
