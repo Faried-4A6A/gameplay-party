303d0fdaba87576b26a3a1ff64d03c56b3f1014e
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-10-02 21:04:23 +0200

    bioscoop show

---
+++ model/overzichtsLogic.php
@@ -0,0 +1,23 @@
+<?php
+require_once('model/dataHandler.php');
+
+class overzichtsLogic {
+
+    public function __construct()
+    {
+        $this->dataHandler = new dataHandler("mysql", "localhost", "gameplayparty", "abuzar", "test");
+    }
+    public function readCatalogus()
+    {
+       $query = 'SELECT * FROM bioscopen';
+       $bioscopen = $this->dataHandler->ReadData($query);
+       return $bioscopen;
+    }
+}
+
+
+
+
+
+
+
