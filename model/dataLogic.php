be6094f2f94fdd89beca107b159887b865f10f9d
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-10-10 10:48:47 +0200

    add bios

--- model/dataLogic.php
+++ model/dataLogic.php
@@ -19,4 +19,13 @@ class dataLogic {
         return $this->dataHandler->ReadData("DESCRIBE gameplayparty.bioscopen");
     }
     
+    public function createBios($bios_naam, $adres, $postcode, $stad, $provincie, $bereikbaar_auto, $bereikbaar_ov, $bereikbaar_fiets, $rolstoeltoegankelijkheid, $voorwaarden)
+    {
+        try {
+            return $this->dataHandler->CreateData("INSERT INTO bioscopen(`bios_naam`,`adres`,`postcode`,`stad`,`provincie`,`bereikbaar_auto`,`bereikbaar_ov`,`bereikbaar_fiets`,`rolstoeltoegankelijkheid`,`voorwaarden`) 
+            VALUES ('$bios_naam', '$adres', '$postcode', '$stad', '$provincie', '$bereikbaar_auto', '$bereikbaar_ov', '$bereikbaar_fiets', '$rolstoeltoegankelijkheid', '$voorwaarden')");
+        } catch (Exeption $e) {
+            throw $e;
+        }
+    }
 }
