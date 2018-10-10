590cffe19b7225c9dd4e8c365ad96cc8b401ec7b
Faried-4A6A <514152@edu.rocmn.nl>
2018-10-10 11:32:58 +0200

    footer

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -1,17 +1,18 @@
 <!-- Footer -->
 <footer class="footer text-center footer-blok">
     <div class="container">
-        <div class="row ">
-            <div class="col-md-4">
-            </div>
-            <div class="col-md-4">
-                <i class="fab fa-facebook w-25 h-25"></i>
-                <i class="fab fa-instagram"></i>
-            </div>
-            <div class="col-md-4">
-            </div>
+        <div class="content-justify-center">
+            <i class="fab fa-2 fa-facebook fa-3x"></i>
+            <i class="fab fa-instagram fa-3x"></i>
         </div>
     </div>
+    <blockquote class="blockquote text-center">
+      <p class="lead">
+        <a class="btn btn-lg" style="color:#009432;" href="http://localhost/overzichtController/overzicht" role="button">Algemene reserveringsvoorwaarden</a>
+        <a class="btn btn-lg" style="color:#009432;" href="http://localhost/overzichtController/overzicht" role="button">Algemene voorwaarden</a>
+      </p>
+      <footer class="blockquote-footer">Copyright &copy; Gameplayparty <cite title="Source Title"><?php echo date("Y"); ?></cite></footer>
+    </blockquote>
 </footer>
 </body>
 </html>
