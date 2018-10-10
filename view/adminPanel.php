61c6b94ff212e5f1cc93fe5a71921185cd5b5fee
Faried-4A6A <514152@edu.rocmn.nl>
2018-10-10 14:52:05 +0200

    dashboard remake

--- view/adminPanel.php
+++ view/adminPanel.php
@@ -1,54 +1,42 @@
 <?php include('partials/header.php'); ?>
 <div class="container-fluid">
       <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link text-primary" href="#">
-                <i class="fas fa-file"></i>
-                  Dashboard <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" style="color: #009432;" href="#">
-                <i class="fas fa-film"></i>
-                  Bioscopen
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" style="color: #009432;" href="#">
-                <i class="fas fa-home"></i>
-                  Home
-                </a>
-              </li>
-            </ul>
-
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" style="color: #009432;" href="#">
-                <i class="fas fa-address-book"></i>
-                Contact
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" style="color: #009432;" href="#">
-                <i class="fab fa-stack-overflow"></i>
-                 Beheer
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10">
+        <main role="main" class="mx-auto">
             <div class="form-row mt-5 font-italic">
                 <label><h1>Dashboard</h1></label>
             </div>
             <hr class="my-4">
-            <button type="button" class="btn btn-lg" style="background-color:#A3CB38;"><a class="nav-link text-white" href="/adminController/addBios">Bioscoop toevoegen</a></button>
-            <button type="button" class="btn btn-lg" style="background-color:#34495e;"><a class="nav-link" style="color:#fff;" href="adminController/showCMS">Aanpassingen</a></button>
+            <div class="card-deck">
+            <div class="card" style="width: 18rem;">
+              <div class="card-body" style="background-color:#A3CB38;">
+                <h5 class="card-title"><button type="button" class="btn btn-lg" style="background-color:#A3CB38;"><a class="nav-link text-white" href="/adminController/addBios">Bioscoop toevoegen</a></button></h5>
+              </div>
+            </div>
 
+            <div class="card" style="width: 18rem;">
+              <div class="card-body" style="background-color:#A3CB38;">
+                <h5 class="card-title"><button type="button" class="btn btn-lg" style="background-color:#A3CB38;"><a class="nav-link" style="color:#fff;" href="adminController/showCMS">Aanpassingen</a></button></h5>
+              </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+              <div class="card-body" style="background-color:#A3CB38;">
+                <h5 class="card-title"><button type="button" class="btn btn-lg" style="background-color:#A3CB38;"><a class="nav-link text-white" href="/adminController/addBios">Bioscoop</a></button></h5>
+              </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+              <div class="card-body" style="background-color:#A3CB38;">
+                <h5 class="card-title"><button type="button" class="btn btn-lg" style="background-color:#A3CB38;"><a class="nav-link" style="color:#fff;" href="adminController/showCMS">Home</a></button></h5>
+              </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+              <div class="card-body" style="background-color:#A3CB38;">
+                <h5 class="card-title"><button type="button" class="btn btn-lg" style="background-color:#A3CB38;"><a class="nav-link text-white" href="/adminController/addBios">Contact</a></button></h5>
+              </div>
+            </div>
+        </div>
         </main>
       </div>
     </div>
