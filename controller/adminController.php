146b6b0664c83a5d62e61fdc3b8c2e9575d23ba5
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-10-10 10:56:13 +0200

    bios toe voegen

--- controller/adminController.php
+++ controller/adminController.php
@@ -20,7 +20,8 @@ class adminController
     function addBios(){
         if (isset($_POST['send'])) {
             $create = $this->dataLogic->createBios($_POST['bios_naam'], $_POST['adres'], $_POST['postcode'], $_POST['stad'], $_POST['provincie'], $_POST['bereikbaar_auto'], $_POST['bereikbaar_ov'], $_POST['bereikbaar_fiets'], $_POST['rolstoeltoegankelijkheid'], $_POST['voorwaarden']);
-            $this->adminPanel();
+            include 'view/addBios.php';
+            echo "<div class='alert alert-success' role='alert'>Bios toegevoegdt </div>";
         } else {
             $form = $this->htmlElements->createForm('', $this->dataLogic->describeForm());
             include 'view/addBios.php';
