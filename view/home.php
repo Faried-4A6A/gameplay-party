35ef44781e696c5de5164f796e995cf25e429369
abuzar-4a6c <abuzar_voetbal@msn.com>
2018-10-17 11:25:24 +0200

    ds

--- view/home.php
+++ view/home.php
@@ -28,9 +28,9 @@
   <hr>
 
   <div class="row">
-      <div class="col-sm-5 col-md-6"><img src="https://via.placeholder.com/650x450?text=bedrijfsFoto" width="650" height="450" alt="..." class="img-thumbnail"></div>
-      <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
+      <div class="col-sm-5 offset-sm-2 col-md-12 my-3 offset-md-0">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      
       </div>
   </div>
   <!-- /.row -->
