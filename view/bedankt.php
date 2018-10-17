e3ea58b61400800cc447d46df01b230ad0e2b4a1
BolleNicht <512576@edu.rocmn.nl>
2018-10-17 11:47:25 +0200

    up to dat

--- view/bedankt.php
+++ view/bedankt.php
@@ -1,12 +1,14 @@
 <?php require_once "partials/header.php"; ?>
 
 <html>
-<body>
-  Wij zijn blij met uw reservering <?php echo $_POST["fname"]; ?>
-</body>
+  <body>
+    Wij zijn blij met uw reservering <?php echo $_POST["fname"],' ' . $_POST["bname"]; ?>
+  </body>
 </html>
-
-
-
+<?php
+  if ($_POST["fdatum"] = ' ') {
+    alert('datum mag niet leeg zijn');
+  }
+  ?>
 
 <?php require_once "partials/footer.php"; ?>
